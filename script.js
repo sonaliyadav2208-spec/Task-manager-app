27325c932718cc4a34a7b28a78697ac5e279f4fd
sonaliyadav2208-spec <sonaliyadav22081998@gmail.com>
2026-05-07 14:41:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// script.js
+
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const taskText = taskInput.value.trim();
+
+  if (taskText === "") {
+    alert("Please enter a task");
+    return;
+  }
+
+  const li = document.createElement("li");
+
+  li.innerHTML = `
+    <span>${taskText}</span>
+    <div class="actions">
+      <button onclick="completeTask(this)">Done</button>
+      <button onclick="deleteTask(this)">Delete</button>
+    </div>
+  `;
+
+  document.getElementById("taskList").appendChild(li);
+
+  taskInput.value = "";
+}
+
+function completeTask(button) {
+  const task = button.parentElement.parentElement;
+  task.classList.toggle("completed");
+}
+
+function deleteTask(button) {
+  const task = button.parentElement.parentElement;
+  task.remove();
+}
